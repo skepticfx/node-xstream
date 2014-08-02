dc75363af02252c0bc5ec89b236f7edae5a1c6f5
skepticfx <ahamed.nafeez@gmail.com>
2014-08-02 21:19:06 +0530

    core: add hookBuffer

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 var through = require('through');
 var Xstream = exports;
 
+// custom_function accepts and returns buffer.
+Xstream.hookBuffer = function(custom_function){
+
+  function t_write(data){
+    data = custom_function(data);
+    var buf = new Buffer(data);
+    this.emit('data', buf);
+  }
+
+  function t_end(){
+    this.emit('end');
+  }
+return through(t_write, t_end);
+}
+
+Xstream.hookString = function(custom_function){
+
+  function t_write(data){
+    var str = data.toString('utf8');
+    str = custom_function(str);
+    var buf = new Buffer(str);
+    this.emit('data', buf);
+  }
+
+  function t_end(){
+    this.emit('end');
+  }
+return through(t_write, t_end);
+}
+
+
 Xstream.hook = function(custom_function){
 
   function t_write(data){
